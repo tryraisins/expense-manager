8a0cb8fb11b65bac9b7651f505662f64c178f158
Try Raisins <36957218+OluwaseunSowemimo@users.noreply.github.com>
2022-08-31 05:14:13 +0100

    added functionality for adding new data and receipt

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -4,6 +4,7 @@ import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import "./Main.css";
 import data from "../../assets/MOCK_DATA.json";
+import receipt from "../../assets/receipt.png";
 
 //MAIN  TABLE COMPONENT
 
@@ -36,12 +37,37 @@ const Main = ({ modal, ToggleModal }) => {
   //TABLE DATA STATE
 
   const [rowData, setRowData] = useState();
+  const [gridApi, setGridApi] = useState();
 
-  //MODAL STATE
-  const [isOpen, setIsOpen] = useState(false);
-  const openModal = () => {
-    setIsOpen(true);
+  //MODAL STATES
+  const [addRowModal, setAddRowModal] = useState(false);
+  const openAddRowModal = () => {
+    setAddRowModal(true);
   };
+  const closeAddRowModal = () => {
+    setAddRowModal(false);
+  };
+
+  const [onRowSelectModal, setOnRowSelectModal] = useState(false);
+  const openOnRowSelectModal = () => {
+    setOnRowSelectModal(true);
+  };
+  const closeOnRowSelectModal = () => {
+    setOnRowSelectModal(false);
+  };
+
+  //ONSELECTEDROW DATA STATES
+  const [selectedRow, setSelectedRow] = useState();
+  const [dateData, setDateData] = useState();
+  const [merchantData, setMerchantData] = useState();
+  const [totalData, setTotalData] = useState();
+  const [paymentMethodData, setPaymentMethodData] = useState();
+  const [frequencyData, setFrequencyData] = useState();
+  const [imageData, setImageData] = useState(null);
+  const [imageUrl, setImageUrl] = useState(receipt);
+  const [onRowClickedState, setOnRowClickedState] = useState(false);
+  const [rowID, setRowID] = useState();
+
   //FORM DATA STATE
   const [newRow, setNewRow] = useState(false);
   const [formData, updateFormData] = useState(FormDataTemplate);
@@ -68,15 +94,9 @@ const Main = ({ modal, ToggleModal }) => {
       setNewRow(false);
     }
 
-    closeModal();
+    closeAddRowModal();
   };
 
-  //
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  //
   const getResult = () => {
     const getInfo = data.map((arr, i) => {
       if (arr.Frequency === "Never") {
@@ -116,14 +136,17 @@ const Main = ({ modal, ToggleModal }) => {
       filter: true,
       floatingFilter: false,
       resizable: true,
+      suppressNavigable: true,
+      cellClass: "no-border",
     };
   }, []);
 
-  const onGridReady = () => {
+  const onGridReady = (params) => {
     const firstData = data.map((obj, i) => {
       return { id: i, ...obj };
     });
     setRowData(firstData);
+    setGridApi(params.api);
   };
   const getRowId = useMemo(() => {
     return (params) => params.data.id;
@@ -135,8 +158,40 @@ const Main = ({ modal, ToggleModal }) => {
     });
     setRowData(firstData);
   }, [newRow]);
-  useEffect(() => {}, [newRow]);
+  useEffect(() => {
+    if (imageData) {
+      setImageUrl(URL.createObjectURL(imageData));
+      const newData = rowData;
+      newData[rowID].Image = imageUrl;
+      setRowData(newData);
+      setImageData(null);
+    }
+  }, [imageData, selectedRow, imageUrl, rowID, rowData]);
+  useEffect(() => {
+    setImageUrl(receipt);
+  }, [onRowClickedState]);
 
+  const getSelectedRowData = () => {
+    setSelectedRow(gridApi.getSelectedRows()[0]);
+
+    setMerchantData(selectedRow.Merchant);
+    setTotalData(selectedRow.Total);
+    setDateData(selectedRow.Date);
+    setPaymentMethodData(selectedRow["Payment Method"]);
+    setFrequencyData(selectedRow.Frequency);
+    setImageUrl(selectedRow.Image);
+    setRowID(selectedRow.id * 1);
+  };
+
+  const onRowClicked = () => {
+    getSelectedRowData();
+    openOnRowSelectModal();
+    if (onRowClickedState) {
+      setOnRowClickedState(false);
+    } else {
+      setOnRowClickedState(true);
+    }
+  };
   return (
     <div>
       <div className='fl w-100  pa2 bg-light-gray tc   bb b--black-20 '>
@@ -147,141 +202,10 @@ const Main = ({ modal, ToggleModal }) => {
         <h4>${Intl.NumberFormat("en-US").format(answer.toFixed(2))}</h4>
       </div>
       <div className='scrollborder fl bg-white' id='style-1'>
-        {/* Modal */}
-        {isOpen && (
-          <>
-            <div className='overlay' />
-            <div className='modal '>
-              <header className='flex justify-around'>
-                <h2 className=' center '>ADD DATA</h2>
-                <button onClick={closeModal} className='close-button  '>
-                  &times;
-                </button>
-              </header>
-              <main className='modal__main'>
-                <form className='pa4 black-80'>
-                  {/* DATE */}
-                  <div className='measure  mb4'>
-                    <label htmlFor='date' className='f6 b db mb2'>
-                      Date
-                    </label>
-                    <input
-                      id='date'
-                      className='input-reset ba b--black-20 pa2 mb2 db w-100'
-                      type='date'
-                      aria-describedby='name-desc'
-                      onChange={handleChange}
-                      name='Date'
-                    />
-                    <small id='name-desc' className='f6 black-60 db mb2'>
-                      Add date of Transaction here
-                    </small>
-                  </div>
-                  {/* MERCHANT */}
-                  <div className='measure  mb4'>
-                    <label htmlFor='merchant' className='f6 b db mb2'>
-                      Merchant
-                    </label>
-                    <input
-                      id='merchant'
-                      className='input-reset ba b--black-20 pa2 h2 mb2 db w-100'
-                      type='text'
-                      aria-describedby='name-desc'
-                      onChange={handleChange}
-                      name='Merchant'
-                    />
-                    <small id='name-desc' className='f6 black-60 db mb2'>
-                      Enter name of Merchant here
-                    </small>
-                  </div>
-
-                  {/* TOTAL */}
-                  <div className='measure  mb4'>
-                    <label htmlFor='total' className='f6 b db mb2'>
-                      Total{" "}
-                      <span className='normal black-60 b'> (In Dollars)</span>
-                    </label>
-
-                    <input
-                      type='number'
-                      prefix='$'
-                      onChange={handleChange}
-                      className='input-reset ba b--black-20 pa2 mb2 db w-100'
-                      name='Total'
-                      id='total'
-                      placeholder='$0.00'
-                    />
-                    <small id='name-desc' className='f6 black-60 db mb2'>
-                      Enter Amount Spent Here
-                    </small>
-                  </div>
-
-                  {/* PAYMENT METHOD */}
-                  <div className='measure mb4'>
-                    <label htmlFor='payment-method' className='f6 b db mb2'>
-                      Payment Method{" "}
-                    </label>
-                    <input
-                      id='payment-method'
-                      className='input-reset ba b--black-20 pa2 mb2 db w-100'
-                      type='text'
-                      aria-describedby='name-desc'
-                      onChange={handleChange}
-                      name='Payment Method'
-                    />
-                    <small id='name-desc' className='f6 black-60 db mb2 '>
-                      Enter Credit Card Type
-                    </small>
-                  </div>
-
-                  {/* FREQUENCY */}
-                  <div className='measure'>
-                    <label htmlFor='frequency' className='f6 b db mb2'>
-                      Frequency
-                    </label>
-                    <select
-                      id='frequency'
-                      name='Frequency'
-                      className='pa2 mb2'
-                      onChange={handleChange}
-                    >
-                      <option value='' default={true}></option>
-                      <option value='Daily'>Daily</option>
-                      <option value='Monthly'>Monthly</option>
-                      <option value='Never' default={true}>
-                        Never
-                      </option>
-                      <option value='Often'>Often</option>
-                      <option value='Once'>Once</option>
-                      <option value='Seldom'>Seldom</option>
-                      <option value='Weekly'>Weekly</option>
-                      <option value='Yearly'>Yearly</option>
-                    </select>
-
-                    <small id='name-desc' className='f6 black-60 db mb2'>
-                      Select Frequency of Transaction Here
-                    </small>
-                  </div>
-
-                  {/* Add Button */}
-                  <div className=' flex  mt5 center'>
-                    <input
-                      className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 tc center'
-                      type='submit'
-                      value='Add Data'
-                      onClick={handleSubmit}
-                    />
-                  </div>
-                </form>
-              </main>
-            </div>
-          </>
-        )}
-
         <div className='bg-white center h-100 w-100 tc relative'>
           <div
-            className='pa2 fixed bottom-0 right-2 z-9999'
-            onClick={openModal}
+            className='pa2 fixed bottom-0 right-2 z-9999 dn db-l'
+            onClick={openAddRowModal}
           >
             <p className='f3  dim pa3 mb2 dib white bg-red br-100 pointer'>
               <i className='fa-solid fa-plus' />
@@ -296,6 +220,8 @@ const Main = ({ modal, ToggleModal }) => {
               columnDefs={columnDefs}
               defaultColDef={defaultColDef}
               ref={gridRef}
+              rowSelection={"single"}
+              onRowClicked={onRowClicked}
               onGridReady={onGridReady}
               animateRows={true}
               getRowId={getRowId}
@@ -303,6 +229,238 @@ const Main = ({ modal, ToggleModal }) => {
           </div>
         </div>
       </div>
+      {/* Modals */}
+      {/* ADD ROW MODAL */}
+      {addRowModal && (
+        <>
+          <div className='overlay' />
+          <div className='addrowmodal '>
+            <header className='flex justify-around'>
+              <h2 className=' center '>ADD DATA</h2>
+              <button onClick={closeAddRowModal} className='close-button  '>
+                &times;
+              </button>
+            </header>
+            <main className='modal__main center'>
+              <form className='pa4 black-80 center'>
+                {/* DATE */}
+                <div className='measure center  mb4'>
+                  <label htmlFor='date' className='f6 b  db mb2'>
+                    DATE
+                  </label>
+                  <input
+                    id='date'
+                    className='input-reset ba b--black-20 pa2 mb2 db w-100'
+                    type='date'
+                    aria-describedby='name-desc'
+                    onChange={handleChange}
+                    name='Date'
+                  />
+                  <small id='name-desc' className='f6 black-60 db mb2'>
+                    Add date of Transaction here
+                  </small>
+                </div>
+                {/* MERCHANT */}
+                <div className='measure  center mb4'>
+                  <label htmlFor='merchant' className='f6 b db mb2'>
+                    MERCHANT
+                  </label>
+                  <input
+                    id='merchant'
+                    className='input-reset ba b--black-20 pa2 h2 mb2 db w-100'
+                    type='text'
+                    aria-describedby='name-desc'
+                    onChange={handleChange}
+                    name='Merchant'
+                  />
+                  <small id='name-desc' className='f6 black-60 db mb2'>
+                    Enter name of Merchant here
+                  </small>
+                </div>
+
+                {/* TOTAL */}
+                <div className='measure center mb4'>
+                  <label htmlFor='total' className='f6 b db mb2'>
+                    TOTAL{" "}
+                    <span className='normal black-60 b'> (In Dollars)</span>
+                  </label>
+
+                  <input
+                    type='number'
+                    prefix='$'
+                    onChange={handleChange}
+                    className='input-reset ba b--black-20 pa2 mb2 db w-100'
+                    name='Total'
+                    id='total'
+                    placeholder='$0.00'
+                  />
+                  <small id='name-desc' className='f6 black-60 db mb2'>
+                    Enter Amount Spent Here
+                  </small>
+                </div>
+
+                {/* PAYMENT METHOD */}
+                <div className='measure center mb4'>
+                  <label htmlFor='payment-method' className='f6 b db mb2'>
+                    PAYMENT METHOD{" "}
+                  </label>
+                  <input
+                    id='payment-method'
+                    className='input-reset ba b--black-20 pa2 mb2 db w-100'
+                    type='text'
+                    aria-describedby='name-desc'
+                    onChange={handleChange}
+                    name='Payment Method'
+                  />
+                  <small id='name-desc' className='f6 black-60 db mb2 '>
+                    Enter Credit Card Type
+                  </small>
+                </div>
+
+                {/* FREQUENCY */}
+                <div className='measure center '>
+                  <label htmlFor='frequency' className='f6 b  db mb2'>
+                    FREQUENCY
+                  </label>
+                  <select
+                    id='frequency'
+                    name='Frequency'
+                    className='pa2 w-100 mb2'
+                    onChange={handleChange}
+                  >
+                    <option value=''></option>
+                    <option value='Daily'>Daily</option>
+                    <option value='Monthly'>Monthly</option>
+                    <option value='Never' default={true}>
+                      Never
+                    </option>
+                    <option value='Often'>Often</option>
+                    <option value='Once'>Once</option>
+                    <option value='Seldom'>Seldom</option>
+                    <option value='Weekly'>Weekly</option>
+                    <option value='Yearly'>Yearly</option>
+                  </select>
+
+                  <small id='name-desc' className='f6 black-60 db mb2'>
+                    Select Frequency of Transaction Here
+                  </small>
+                </div>
+
+                {/* Add Button */}
+                <div className=' flex  mt3 center'>
+                  <input
+                    className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 tc center'
+                    type='submit'
+                    value='ADD DATA'
+                    onClick={handleSubmit}
+                  />
+                </div>
+              </form>
+            </main>
+          </div>
+        </>
+      )}
+      {/* ON ROW SELECT MODAL */}
+      {onRowSelectModal && (
+        <>
+          <div className='overlay  dn db-l'></div>
+          <div className='rowselectmodal  ph2 dn di-l'>
+            <main className=' db min-h-100 center '>
+              <article className=' db min-h-100 '>
+                <div class='fl di pl3 w-50'>
+                  <h2>EXPENSES</h2>
+                  <ul class='list  black-50 pl0'>
+                    <li class='pa3  '>
+                      <b class='db f5 mb1 '>Merchant</b>
+                      <span className='f5 db pv1 ph2 lh-copy measure ba b--dashed b--black-30'>
+                        {merchantData}
+                      </span>
+                    </li>
+                    <li class='pa3 '>
+                      <b class='db f5 mb1'>Total</b>
+                      <span className='f5 db pv1 ph2 lh-copy measure ba b--dashed b--black-30'>
+                        {totalData}
+                      </span>
+                    </li>
+                    <li class='pa3 '>
+                      <b class='db f5 mb1 '>Date</b>
+                      <span className='f5 db pv1 ph2 lh-copy measure ba b--dashed b--black-30'>
+                        {dateData}
+                      </span>
+                    </li>
+                    <li class='pa3 '>
+                      <b class='db f5 mb1 '>Payment Method</b>
+                      <span className='f5 db pv1 ph2 lh-copy measure ba b--dashed b--black-30'>
+                        {paymentMethodData}
+                      </span>
+                    </li>
+                    <li class='pa3 '>
+                      <b class='db f5 mb1 '>Frequency</b>
+                      <span className='f5 db pv1 ph2 lh-copy measure ba b--dashed b--black-30'>
+                        {frequencyData}
+                      </span>
+                    </li>
+
+                    <li class='pa3 '>
+                      <div class='measure'>
+                        <label for='name' class='f6 b db mb2'>
+                          Comments
+                        </label>
+                        <input
+                          id='name'
+                          class='input-reset ba b--black-20 pa2 mb2 h3 db w-100'
+                          type='text'
+                          aria-describedby='name-desc'
+                        />
+                      </div>
+                    </li>
+
+                    <li class='fl  pa1 w-100 justify-between'>
+                      <p class='f5 fl b  link dim br3 ph3 pv2   dib white bg-blue'>
+                        Save
+                      </p>
+                      <p
+                        class='f5 fr b link dim br3 ph3 pv2  dib blue bg-light-gray pointer'
+                        onClick={closeOnRowSelectModal}
+                      >
+                        Cancel
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+                <div class='fl di w-50  pa2 ma1  br2 ba  b--dashed b--black-30  center '>
+                  <article class='  overflow-auto dark-gray min-w-100 '>
+                    <div className=' h-25'>
+                      <div class='flex mt1'>
+                        <input
+                          accept='image/*'
+                          type='file'
+                          id='select-image'
+                          className='dn'
+                          onChange={(e) => setImageData(e.target.files[0])}
+                        />
+                        <label htmlFor='select-image' className='center'>
+                          <p className='f6 dim link br-pill ph3 pv2 mb2 dib white bg-blue'>
+                            Select Receipt
+                          </p>
+                        </label>
+                      </div>
+                    </div>
+                    <div className=''>
+                      <p className='f4 b'>Receipt:</p>
+                      <img
+                        src={imageUrl}
+                        className='  br2  br--top'
+                        alt='Receipt'
+                      />
+                    </div>
+                  </article>
+                </div>
+              </article>
+            </main>
+          </div>
+        </>
+      )}
     </div>
   );
 };
