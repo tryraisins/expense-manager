75842cd61afa9584c0f48390dfeb649e188c69d5
Try Raisins <36957218+OluwaseunSowemimo@users.noreply.github.com>
2022-08-31 07:31:34 +0100

    added profile page

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -2,7 +2,7 @@ import profile from "../../assets/profile.webp";
 
 const Profile = () => {
   return (
-    <article className='mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10'>
+    <article className=' center h-50 w-25 bg-white br3 pa3 pa4-ns mv3 ba b--black-10'>
       <div className='tc'>
         <img
           src={profile}
@@ -10,14 +10,20 @@ const Profile = () => {
           title='Profile'
           alt='Profile Pic'
         />
-        <h1 className='f4'>John Smith</h1>
+        <h1 className='f3'>John Smith</h1>
         <hr className='mw3 bb bw1 b--black-10' />
       </div>
-      <div className='lh-copy measure center f6 black-70'>
-        <p className=' f5 mt0'>Business Analyst in Accounting Department</p>
-        <p className=' f5 mt0'>
+      <div className='lh-copy measure center mt-5 f6 tc black'>
+        <p className=' f4 mt3'>
+          {" "}
+          <span className='b'> Job Description:</span> Business Analyst
+        </p>
+        <p className=' f4 mt3'>
+          <span className='b'> Department:</span> Accounting
+        </p>
+        <p className=' f4 mt3'>
           <i className='fa-solid fa-location-crosshairs mr1' />
-          Nigeria
+          <span className='b'> Location:</span> Nigeria
         </p>
       </div>
     </article>
